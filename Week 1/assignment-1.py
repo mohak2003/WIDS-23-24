80b14b122a636f2b783a6921b6ef60069221d0d2
mohak2003 <98387803+mohak2003@users.noreply.github.com>
2023-12-11 08:00:50 +0000

    assignment-1.py uploaded

---
+++ Week 1/assignment-1.py
@@ -0,0 +1,19 @@
+# Write a Python function to check whether a number is in a given range.
+def test_range(n):
+    # Your code here
+
+# Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
+def string_test(s):
+    # Your code here
+
+# Write a Python program to count the number of even and odd numbers from a series of numbers.
+def count(numbers):
+    # Your code here
+
+# Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+def print_numbers():
+    # Your code here
+
+# Write a Python program to print the Fibonacci series between 0 to 50
+def fibonacci():
+    # Your code here
